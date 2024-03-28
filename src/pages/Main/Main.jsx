7de4eb567bf045ea92a6fc7de9c29bb09226e68c
Jimmy MARTEL <jimmy.martel@epitech.eu>
2024-03-28 18:06:27 +0100

    Fix mobile design

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -6,7 +6,7 @@ const Main = () => {
   return (
     <main className={"p-8 flex flex-col gap-4"}>
       <h1 className="text-4xl text-center">JWT Decoder</h1>
-      <div className="flex md:flex-wrap w-full gap-4 justify-center  h-screen min-h-screen">
+      <div className="flex flex-col md:flex-wrap w-full gap-4 justify-center h-screen min-h-screen">
         <Encoded></Encoded>
         <Decoded></Decoded>
       </div>
